209ea5c839d67f57bf9b45b2601bab05f4992771
Robert <robert.rachubinski@gmail.com>
2022-08-09 23:15:52 +0200

    def get_author_key

--- searcher/urls.py
+++ searcher/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 app_name = 'searcher'
 urlpatterns = [
-    path('', views.search, name='home'),
+    path('', views.books_by_author, name='home'),
     path('a/', views.find_author, name='author_finder'),
 ]
--- searcher/views.py
+++ searcher/views.py
@@ -23,3 +23,30 @@ def find_author(request):
         authors = response.json()
         print(authors)
     return render(request, 'searcher/author_finder.html', {'authors': authors})
+
+
+
+def books_by_author(request):
+    author_key = ""
+    if 'author' in request.GET:
+        author_key = get_author_key(request)
+        print('hej' + author_key)
+    return render(request, 'searcher/home.html', {'author_key': author_key})
+
+
+
+def get_author_key(request):
+    author = str(request.GET['author'])
+    author = author.replace(' ', '+')
+    url = f'https://openlibrary.org/search/authors.json?q="{author}"'
+    response = requests.get(url)
+    author_key = response.json()['docs'][0]['key']
+    print(author_key)
+    return author_key
+
+
+def get_author_info(author_key):
+    url = f'https://openlibrary.org/authors/{author_key}.json'
+    response = requests.get(url)
+    author_info = response.json()
+    return author_info
