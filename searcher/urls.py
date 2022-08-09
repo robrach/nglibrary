af1d446ee94dbe1cf31947977023f6fcad8a1365
Robert <robert.rachubinski@gmail.com>
2022-08-09 22:06:52 +0200

    Now can find the author via author_finder.html

--- searcher/urls.py
+++ searcher/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'searcher'
 urlpatterns = [
     path('', views.search, name='home'),
+    path('a/', views.find_author, name='author_finder'),
 ]
--- searcher/views.py
+++ searcher/views.py
@@ -5,8 +5,21 @@ import requests
 def search(request):
     author = {}
     if 'author_id' in request.GET:
-        author_id = request.GET['author_id']
+        author_id = str(request.GET['author_id'])
         url = f'https://openlibrary.org/authors/{author_id}.json'
         response = requests.get(url)
         author = response.json()
     return render(request, 'searcher/home.html', {'author': author})
+
+
+def find_author(request):
+    authors = {}
+    if 'author' in request.GET:
+        author = str(request.GET['author'])
+        author = author.replace(' ', '+')
+        print(author)
+        url = f'https://openlibrary.org/search/authors.json?q={author}'
+        response = requests.get(url)
+        authors = response.json()
+        print(authors)
+    return render(request, 'searcher/author_finder.html', {'authors': authors})
