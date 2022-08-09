00d8a23b022f0056297e56d8dc65d4bf601c58cf
Robert <robert.rachubinski@gmail.com>
2022-08-09 23:47:07 +0200

    def get_author_books

--- searcher/views.py
+++ searcher/views.py
@@ -29,24 +29,33 @@ def find_author(request):
 
 
 def books_by_author(request):
-    author_key = ""
+    author_key = []
+    author_work_count = 0
     author_info = {}
+    author_books = {}
     if 'author' in request.GET:
-        author_key = get_author_key(request)
+        content = get_author_key_and_work_count(request)
+        author_key = content[0]
+        author_work_count = content[1]
         author_info = get_author_info(author_key)
+        author_books = get_author_books(author_key, author_work_count)
     return render(request, 'searcher/home.html', {
         'author_key': author_key,
+        'author_work_count': author_work_count,
         'author_info': author_info,
+        'author_books': author_books,
     })
 
 
-def get_author_key(request):
+def get_author_key_and_work_count(request):
     author = str(request.GET['author'])
     author = author.replace(' ', '+')
     url = f'https://openlibrary.org/search/authors.json?q="{author}"'
     response = requests.get(url)
-    author_key = response.json()['docs'][0]['key']
-    return author_key
+    content = response.json()
+    author_key = content['docs'][0]['key']
+    author_work_count = content['docs'][0]['work_count']
+    return author_key, author_work_count
 
 
 def get_author_info(author_key):
@@ -54,3 +63,10 @@ def get_author_info(author_key):
     response = requests.get(url)
     author_info = response.json()
     return author_info
+
+
+def get_author_books(author_key, author_work_count):
+    url = f'https://openlibrary.org/authors/{author_key}/works.json?limit={author_work_count}'
+    response = requests.get(url)
+    author_books = response.json()
+    return author_books
